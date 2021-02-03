da989e2af00afb040d014691669403f917bb8f3a
jhammelm <jhammelm@mit.edu>
2021-02-03 11:39:08 -0500

    remove redundant files

--- run_ASCL1vsCTCF_DeepAccess.sh
+++ run_ASCL1vsCTCF_DeepAccess.sh
@@ -1,12 +1,27 @@
 mkdir test
 
-python DeepAccessTrainTransfer.py -beds  data/mm10_trimmed_ASCL1.bed data/mm10_trimmed_CTCF.bed \
+#example train from bed file
+python src/DeepAccessTrainTransfer.py -beds  data/mm10_trimmed_ASCL1.bed data/mm10_trimmed_CTCF.bed \
        -l ASCL1 CTCF -c ASCL1vsCTCF ASCL1vsNone CTCFvsNone  \
        -ref /archive/gl/shared/genomes/mm10/mm10.fa \
-       -g /archive/gl/shared/user/jhammelm/old_cluster/jhammelm/genomes/atac_mm10/mm10/mm10.chrom.sizes \
-       -out test/ASCL1vsCTCF -nepochs 1 -motifDB HMv11_MOUSE.txt
+       -g default/mm10.chrom.sizes \
+       -out test/ASCL1vsCTCF -nepochs 1 -motifDB default/HMv11_MOUSE.txt
 
-python DeepAccessInterpret.py -trainDir test/ASCL1vsCTCF \
+#example run differential per-nucleotide visualization on fasta input
+python src/DeepAccessInterpret.py -trainDir test/ASCL1vsCTCF \
        -l ASCL1 CTCF -c ASCL1vsCTCF \
-       -fastas test/ASCL1vsCTCF_example.fa -saliency -vis
+       -fastas data/ASCL1vsCTCF_example.fa -saliency -vis
+
+#example run test motif spacing
+python src/DeepAccessInterpret.py -trainDir test/ASCL1vsCTCF \
+       -l ASCL1 CTCF -c ASCL1vsCTCF \
+       -patternFA data/ASCL1_space.fa 
+
+#example train from fasta
+python src/DeepAccessTrainTransfer.py --fasta data/ASCL1vsCTCF_train.fa  \
+       -fasta_labels data/ASCL1vsCTCF_train.txt \
+       -l ASCL1 CTCF -c ASCL1vsCTCF \
+       -ref /archive/gl/shared/genomes/mm10/mm10.fa \
+       -g default/mm10.chrom.sizes  \
+       -out test/ASCL1vsCTCF_from_fasta -nepochs 1 -motifDB default/HMv11_MOUSE.txt
 
