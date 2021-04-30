f6f6509182e70ec53680fa7fb119f1afddbaf65f
jhammelm <jhammelm@mit.edu>
2021-04-30 12:17:32 -0400

    make prediction for tensorflow2

--- src/CNN.py
+++ src/CNN.py
@@ -1,80 +1,116 @@
-import keras 
+import keras
 import numpy as np
 from keras.models import Model, load_model, Sequential
-from keras.layers import Conv1D, Dense, Reshape, Dropout, LSTM, GlobalMaxPooling1D, MaxPooling1D, Flatten, Input, Concatenate
+from keras.layers import (
+    Conv1D,
+    Dense,
+    Reshape,
+    Dropout,
+    LSTM,
+    GlobalMaxPooling1D,
+    MaxPooling1D,
+    Flatten,
+    Input,
+    Concatenate,
+)
 import tensorflow as tf
 from keras import optimizers
 
-#CNN code for motif convolutional layer
+# CNN code for motif convolutional layer
 # taken from https://github.com/uci-cbcl/DanQ
-class CNN():
-    def __init__(self,model_layers,seq_shape,out_shape=2,conv_filter_number=100,conv_filter_size=20):
-        model=Sequential()
+class CNN:
+    def __init__(
+        self,
+        model_layers,
+        seq_shape,
+        out_shape=2,
+        conv_filter_number=100,
+        conv_filter_size=20,
+    ):
+        model = Sequential()
         for i, layer in enumerate(model_layers):
-            if layer == 'conv':
+            if layer == "conv":
                 if i == 0:
-                    HOMER_motifs = list(np.load('homer_matrix.npy',allow_pickle=True))
-                    filter_len = max([HOMER_motifs[k].shape[0] for k in range(len(HOMER_motifs))])
-                    conv_layer = Conv1D(input_shape=seq_shape,
-                                        filters=len(HOMER_motifs)*2,
-                                        kernel_size=35,
-                                        padding="valid",
-                                        activation="relu",
-                                        strides=1)
+                    HOMER_motifs = list(np.load("homer_matrix.npy", allow_pickle=True))
+                    filter_len = max(
+                        [HOMER_motifs[k].shape[0] for k in range(len(HOMER_motifs))]
+                    )
+                    conv_layer = Conv1D(
+                        input_shape=seq_shape,
+                        filters=len(HOMER_motifs) * 2,
+                        kernel_size=35,
+                        padding="valid",
+                        activation="relu",
+                        strides=1,
+                    )
                     model.add(conv_layer)
                     conv_weights = conv_layer.get_weights()
 
-                    reverse_motifs = [HOMER_motifs[j][::-1,::-1] for j in range(len(HOMER_motifs))]
+                    reverse_motifs = [
+                        HOMER_motifs[j][::-1, ::-1] for j in range(len(HOMER_motifs))
+                    ]
                     HOMER_motifs = HOMER_motifs + reverse_motifs
 
                     for j in range(len(HOMER_motifs)):
-                            m = HOMER_motifs[j][::-1,:]
-                            w = m.shape[0]
-                            conv_weights[0][:,:,j] = 0
-                            start = np.random.randint(low=3, high=35-w-3+1)
-                            conv_weights[0][start:(start+w),:,j] = m - 0.25
-                            conv_weights[1][j] = np.random.uniform(low=-1.0,high=0.0)
+                        m = HOMER_motifs[j][::-1, :]
+                        w = m.shape[0]
+                        conv_weights[0][:, :, j] = 0
+                        start = np.random.randint(low=3, high=35 - w - 3 + 1)
+                        conv_weights[0][start : (start + w), :, j] = m - 0.25
+                        conv_weights[1][j] = np.random.uniform(low=-1.0, high=0.0)
 
                     conv_layer.set_weights(conv_weights)
-                    conv_layer.trainable=False
+                    conv_layer.trainable = False
                 else:
-                    model.add(Conv1D(conv_filter_number,conv_filter_size,activation='relu',padding='same'))
-            if layer == 'globalpool':
+                    model.add(
+                        Conv1D(
+                            conv_filter_number,
+                            conv_filter_size,
+                            activation="relu",
+                            padding="same",
+                        )
+                    )
+            if layer == "globalpool":
                 model.add(GlobalMaxPooling1D())
-            if layer == 'maxpool':
-                model.add(MaxPooling1D(3,1))
-            if layer == 'LSTM':
-                model.add(LSTM(16,return_sequences=True))
-            if layer == 'dense':
+            if layer == "maxpool":
+                model.add(MaxPooling1D(3, 1))
+            if layer == "LSTM":
+                model.add(LSTM(16, return_sequences=True))
+            if layer == "dense":
                 model.add(Dropout(0.1))
-                model.add(Dense(128,activation='relu'))
-            
-        if 'dense' not in model_layers and 'globalpool' not in model_layers:
+                model.add(Dense(128, activation="relu"))
+
+        if "dense" not in model_layers and "globalpool" not in model_layers:
             model.add(Flatten())
         model.add(Dropout(0.1))
-        model.add(Dense(out_shape,activation='sigmoid'))
-        adam = optimizers.Adam(lr=1e-4,clipnorm=0.5,decay=(1e-4/100.0))
-        model.compile(optimizer=adam,loss='binary_crossentropy',metrics=['accuracy'])
+        model.add(Dense(out_shape, activation="sigmoid"))
+        adam = optimizers.Adam(lr=1e-4, clipnorm=0.5, decay=(1e-4 / 100.0))
+        model.compile(optimizer=adam, loss="binary_crossentropy", metrics=["accuracy"])
         self.model = model
 
-    def train(self,X,y,sample_weights,n_epochs=100):
-        callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', 
-                                                   patience=3),
-                     keras.callbacks.History()]
-        history = self.model.fit(x=X,
-                                 y=y,epochs=n_epochs,
-                                 shuffle=True,
-                                 validation_split=0.2,
-                                 batch_size=100,verbose=1,
-                                 callbacks=callbacks,
-                                 sample_weight = sample_weights)
+    def train(self, X, y, sample_weights, n_epochs=100):
+        callbacks = [
+            keras.callbacks.EarlyStopping(monitor="val_loss", patience=3),
+            keras.callbacks.History(),
+        ]
+        history = self.model.fit(
+            x=X,
+            y=y,
+            epochs=n_epochs,
+            shuffle=True,
+            validation_split=0.2,
+            batch_size=100,
+            verbose=1,
+            callbacks=callbacks,
+            sample_weight=sample_weights,
+        )
         return history
 
-    def save(self,h5file):
+    def save(self, h5file):
         self.model.save(h5file)
 
-    def predict(self,X):
+    def predict(self, X):
         return self.model.predict(X)
 
-    def error(self,X,y):
-        return np.linalg.norm(y-self.model.predict(X))
+    def error(self, X, y):
+        return np.linalg.norm(y - self.model.predict(X))
--- src/ExpectedPatternEffect.py
+++ src/ExpectedPatternEffect.py
@@ -1,76 +1,82 @@
-from scipy.stats import wilcoxon,t,norm
+from scipy.stats import wilcoxon, t, norm
 import numpy as np
 from ensemble_utils import *
 
-def motif2test(motiffile,backgroundfile):
-    motifs = open(motiffile).read().split('>')[1:]
+
+def motif2test(motiffile, backgroundfile):
+    motifs = open(motiffile).read().split(">")[1:]
     motifmats = {}
     for motif in motifs:
-        motifname = motif.strip().split('\n')[0]
-        motiflines = motif.strip().split('\n')[1:]
-        
-        motif_mat = np.zeros((len(motiflines),4))
-        for i,line in enumerate(motiflines):
-            counts = np.array([float(c) for c in line.split('\t')])
-            motif_mat[i,:] = counts
+        motifname = motif.strip().split("\n")[0]
+        motiflines = motif.strip().split("\n")[1:]
+
+        motif_mat = np.zeros((len(motiflines), 4))
+        for i, line in enumerate(motiflines):
+            counts = np.array([float(c) for c in line.split("\t")])
+            motif_mat[i, :] = counts
         motifmats[motifname] = motif_mat
     names = []
     all_backgrounds = []
     for motifname in motifmats.keys():
         motif = motifmats[motifname]
         backgrounds = fa_to_onehot(backgroundfile)
-        start = int(backgrounds.shape[1]/2 - motif.shape[0]/2)
+        start = int(backgrounds.shape[1] / 2 - motif.shape[0] / 2)
         for bi in range(backgrounds.shape[0]):
             for pos in range(motif.shape[0]):
-                consensus_char = np.argmax(motif[pos,:])
-                backgrounds[bi,pos+start,:] = 0
-                backgrounds[bi,pos+start,consensus_char] = 1.0
-            names.append(motifname+'_EPEDistribution_'+str(bi))
+                consensus_char = np.argmax(motif[pos, :])
+                backgrounds[bi, pos + start, :] = 0
+                backgrounds[bi, pos + start, consensus_char] = 1.0
+            names.append(motifname + "_EPEDistribution_" + str(bi))
         all_backgrounds.append(backgrounds)
-        
-    null_backgrounds = fa_to_onehot(backgroundfile)
-    return np.concatenate(all_backgrounds,axis=0),np.array(null_backgrounds),names
 
-def fasta2test(fastafile,backgroundfile):
-    fastanames = [f.split('\n')[0] for f in open(fastafile).read().split('>')[1:]]
+    null_backgrounds = fa_to_onehot(backgroundfile)
+    return np.concatenate(all_backgrounds, axis=0), np.array(null_backgrounds), names
+
+
+def fasta2test(fastafile, backgroundfile):
+    fastanames = [f.split("\n")[0] for f in open(fastafile).read().split(">")[1:]]
     fastas = fa_to_onehot(fastafile)
     names = []
     all_backgrounds = []
-    for fi,fastaname in enumerate(fastanames):
-        fasta = fastas[fi,:,:]
+    for fi, fastaname in enumerate(fastanames):
+        fasta = fastas[fi, :, :]
         backgrounds = fa_to_onehot(backgroundfile)
-        start = int(backgrounds.shape[1]/2 - fasta.shape[0]/2)
+        start = int(backgrounds.shape[1] / 2 - fasta.shape[0] / 2)
         for bi in range(backgrounds.shape[0]):
             for pos in range(fasta.shape[0]):
-                #if fasta is not N replace background,
+                # if fasta is not N replace background,
                 # otherwise keep background the same
-                if np.max(fasta[pos,:]) != 0.25:
-                    consensus_char = np.argmax(fasta[pos,:])
-                    backgrounds[bi,pos+start,:] = 0
-                    backgrounds[bi,pos+start,consensus_char] = 1.0
-            names.append(fastaname+'_EPEDistribution_'+str(bi))
+                if np.max(fasta[pos, :]) != 0.25:
+                    consensus_char = np.argmax(fasta[pos, :])
+                    backgrounds[bi, pos + start, :] = 0
+                    backgrounds[bi, pos + start, consensus_char] = 1.0
+            names.append(fastaname + "_EPEDistribution_" + str(bi))
         all_backgrounds.append(backgrounds)
     null_backgrounds = fa_to_onehot(backgroundfile)
-    return np.concatenate(all_backgrounds,axis=0),np.array(null_backgrounds),names
+    return np.concatenate(all_backgrounds, axis=0), np.array(null_backgrounds), names
 
-def rank_ratio_statistic(num,denom):
-    sorted_by_ranks = np.argsort(np.abs(np.log2(num/denom)))
+
+def rank_ratio_statistic(num, denom):
+    sorted_by_ranks = np.argsort(np.abs(np.log2(num / denom)))
     Wp = 0
     Wn = 0
     n = num.shape[0]
-    for rank,ri in enumerate(sorted_by_ranks):
+    for rank, ri in enumerate(sorted_by_ranks):
         if np.sign(num[ri] - denom[ri]) == 1:
-            Wp += (rank+1)
+            Wp += rank + 1
         elif np.sign(num[ri] - denom[ri]) == -1:
-            Wn += (rank+1)
-    W = min(Wp,Wn)
-    return norm.cdf(x=(W - n*(n+1)/4)/np.sqrt(n*(n+1)*(2*n+1)/24))*2
-                   
-def ExpectedPatternEffect(predict_function,class_ind,X_p,X,seqsets):
+            Wn += rank + 1
+    W = min(Wp, Wn)
+    return (
+        norm.cdf(x=(W - n * (n + 1) / 4) / np.sqrt(n * (n + 1) * (2 * n + 1) / 24)) * 2
+    )
+
+
+def ExpectedPatternEffect(predict_function, class_ind, X_p, X, seqsets):
     fx_p = predict_function(X_p)
     fx = predict_function(X)
-    patternseqs = np.array([s.split('_EPEDistribution')[0] for s in seqsets])
-    patterns = list(sorted(set([s.split('_EPEDistribution')[0] for s in seqsets])))
+    patternseqs = np.array([s.split("_EPEDistribution")[0] for s in seqsets])
+    patterns = list(sorted(set([s.split("_EPEDistribution")[0] for s in seqsets])))
     stats = []
     pvals = []
     ratio_pvals = []
@@ -79,37 +85,44 @@ def ExpectedPatternEffect(predict_function,class_ind,X_p,X,seqsets):
     for pattern in patterns:
         ind_pattern_seqs = np.where(pattern == patternseqs)[0]
         if len(class_ind) == 1:
-            num = fx_p[ind_pattern_seqs,class_ind[0]].reshape((-1,))
-            denom = fx[:,class_ind[0]].reshape((-1,))
+            num = fx_p[ind_pattern_seqs, class_ind[0]].reshape((-1,))
+            denom = fx[:, class_ind[0]].reshape((-1,))
         else:
-            num = np.mean([fx_p[ind_pattern_seqs,ind] for ind in class_ind],
-                          axis=0).reshape((-1,))
-            denom = np.mean([fx[:,ind] for ind in class_ind],
-                            axis=0).reshape((-1,))
-        fc = num/denom
-        ratio_pvals.append(rank_ratio_statistic(num,denom))
-        stat,pval = wilcoxon(num,
-                             denom)
+            num = np.mean(
+                [fx_p[ind_pattern_seqs, ind] for ind in class_ind], axis=0
+            ).reshape((-1,))
+            denom = np.mean([fx[:, ind] for ind in class_ind], axis=0).reshape((-1,))
+        fc = num / denom
+        ratio_pvals.append(rank_ratio_statistic(num, denom))
+        stat, pval = wilcoxon(num, denom)
 
         EPEs.append(np.mean(np.log2(fc)))
         pvals.append(pval)
-        adj_pvals.append(pval*len(patterns))
+        adj_pvals.append(pval * len(patterns))
         stats.append(stat)
-    return fx_p,fx,{"Pattern":patterns,
-                    "ExpectedPatternEffect":EPEs,
-                    "Significance":ratio_pvals,
-                    "AdjustedSignificance":[r*len(patterns) for r in ratio_pvals],
-                    "WilcoxonTestStatistic":stats,
-                    "WilcoxonSignificance":pvals,
-                    "WilcoxonSignificanceAdj":adj_pvals}
+    return (
+        fx_p,
+        fx,
+        {
+            "Pattern": patterns,
+            "ExpectedPatternEffect": EPEs,
+            "Significance": ratio_pvals,
+            "AdjustedSignificance": [r * len(patterns) for r in ratio_pvals],
+            "WilcoxonTestStatistic": stats,
+            "WilcoxonSignificance": pvals,
+            "WilcoxonSignificanceAdj": adj_pvals,
+        },
+    )
 
-def DifferentialExpectedPatternEffect(predict_function,
-                                      class1_ind,class2_ind,
-                                      X_p,X,seqsets):
-    fx_p,fx,c1_EPEdata = ExpectedPatternEffect(predict_function,
-                                                     class1_ind,X_p,X,seqsets)
-    patternseqs = np.array([s.split('_EPEDistribution')[0] for s in seqsets])
-    patterns = list(set(sorted([s.split('_EPEDistribution')[0] for s in seqsets])))
+
+def DifferentialExpectedPatternEffect(
+    predict_function, class1_ind, class2_ind, X_p, X, seqsets
+):
+    fx_p, fx, c1_EPEdata = ExpectedPatternEffect(
+        predict_function, class1_ind, X_p, X, seqsets
+    )
+    patternseqs = np.array([s.split("_EPEDistribution")[0] for s in seqsets])
+    patterns = list(set(sorted([s.split("_EPEDistribution")[0] for s in seqsets])))
     stats = []
     pvals = []
     adj_pvals = []
@@ -118,35 +131,139 @@ def DifferentialExpectedPatternEffect(predict_function,
     for pattern in patterns:
         ind_pattern_seqs = np.where(pattern == patternseqs)[0]
         if len(class1_ind) == 1:
-            num = fx_p[ind_pattern_seqs,class1_ind].reshape((-1,))
-            denom = fx[:,class1_ind].reshape((-1,))
+            num = fx_p[ind_pattern_seqs, class1_ind].reshape((-1,))
+            denom = fx[:, class1_ind].reshape((-1,))
         else:
-            num = np.mean([fx_p[ind_pattern_seqs,ind] for ind in class1_ind],
-                          axis=0).reshape((-1,))
-            denom = np.mean([fx[:,ind] for ind in class1_ind],
-                            axis=0).reshape((-1,))
-        fc1 = num/denom
+            num = np.mean(
+                [fx_p[ind_pattern_seqs, ind] for ind in class1_ind], axis=0
+            ).reshape((-1,))
+            denom = np.mean([fx[:, ind] for ind in class1_ind], axis=0).reshape((-1,))
+        fc1 = num / denom
         if len(class2_ind) == 1:
-            num = fx_p[ind_pattern_seqs,class2_ind].reshape((-1,))
-            denom = fx[:,class2_ind].reshape((-1,))
+            num = fx_p[ind_pattern_seqs, class2_ind].reshape((-1,))
+            denom = fx[:, class2_ind].reshape((-1,))
         else:
-            num = np.mean([fx_p[ind_pattern_seqs,ind] for ind in class2_ind],
-                          axis=0).reshape((-1,))
-            denom = np.mean([fx[:,ind] for ind in class2_ind],
-                            axis=0).reshape((-1,))
-        fc2 = num/denom
-        stat,pval = wilcoxon(fc1,fc2)
+            num = np.mean(
+                [fx_p[ind_pattern_seqs, ind] for ind in class2_ind], axis=0
+            ).reshape((-1,))
+            denom = np.mean([fx[:, ind] for ind in class2_ind], axis=0).reshape((-1,))
+        fc2 = num / denom
+        stat, pval = wilcoxon(fc1, fc2)
 
-        DiffEPEs.append(np.mean(np.log2(fc1/fc2)))
-        ratio_pvals.append(rank_ratio_statistic(fc1,fc2))
-        
+        DiffEPEs.append(np.mean(np.log2(fc1 / fc2)))
+        ratio_pvals.append(rank_ratio_statistic(fc1, fc2))
+
         pvals.append(pval)
-        adj_pvals.append(pval*len(patterns))
+        adj_pvals.append(pval * len(patterns))
         stats.append(stat)
-    return fx_p,fx,{"Pattern":patterns,
-                    "DifferentialExpectedPatternEffect":DiffEPEs,
-                    "Significance":ratio_pvals,
-                    "AdjustedSignificance":[r*len(patterns) for r in ratio_pvals],
-                    "WilcoxonTestStatistic":stats,
-                    "WilcoxonSignificance":pvals,
-                    "WilcoxonSignificanceAdj":adj_pvals}
+    return (
+        fx_p,
+        fx,
+        {
+            "Pattern": patterns,
+            "DifferentialExpectedPatternEffect": DiffEPEs,
+            "Significance": ratio_pvals,
+            "AdjustedSignificance": [r * len(patterns) for r in ratio_pvals],
+            "WilcoxonTestStatistic": stats,
+            "WilcoxonSignificance": pvals,
+            "WilcoxonSignificanceAdj": adj_pvals,
+        },
+    )
+
+def SubtractDifferentialExpectedPatternEffect(
+    predict_function, class1_ind, class2_ind, X_p, X, seqsets
+):
+    fx_p, fx, c1_EPEdata = ExpectedPatternEffect(
+        predict_function, class1_ind, X_p, X, seqsets
+    )
+    patternseqs = np.array([s.split("_EPEDistribution")[0] for s in seqsets])
+    patterns = list(set(sorted([s.split("_EPEDistribution")[0] for s in seqsets])))
+    stats = []
+    pvals = []
+    adj_pvals = []
+    ratio_pvals = []
+    DiffEPEs = []
+    for pattern in patterns:
+        ind_pattern_seqs = np.where(pattern == patternseqs)[0]
+        if len(class1_ind) == 1:
+            num = fx_p[ind_pattern_seqs, class1_ind].reshape((-1,))
+            denom = fx[:, class1_ind].reshape((-1,))
+        else:
+            num = np.mean(
+                [fx_p[ind_pattern_seqs, ind] for ind in class1_ind], axis=0
+            ).reshape((-1,))
+            denom = np.mean([fx[:, ind] for ind in class1_ind], axis=0).reshape((-1,))
+        fc1 = num / denom
+        if len(class2_ind) == 1:
+            num = fx_p[ind_pattern_seqs, class2_ind].reshape((-1,))
+            denom = fx[:, class2_ind].reshape((-1,))
+        else:
+            num = np.mean(
+                [fx_p[ind_pattern_seqs, ind] for ind in class2_ind], axis=0
+            ).reshape((-1,))
+            denom = np.mean([fx[:, ind] for ind in class2_ind], axis=0).reshape((-1,))
+        fc2 = num / denom
+        stat, pval = wilcoxon(fc1, fc2)
+
+        DiffEPEs.append(np.mean(fc1 - fc2))
+        ratio_pvals.append(rank_ratio_statistic(fc1, fc2))
+
+        pvals.append(pval)
+        adj_pvals.append(pval * len(patterns))
+        stats.append(stat)
+    return (
+        fx_p,
+        fx,
+        {
+            "Pattern": patterns,
+            "DifferentialExpectedPatternEffect": DiffEPEs,
+            "Significance": ratio_pvals,
+            "AdjustedSignificance": [r * len(patterns) for r in ratio_pvals],
+            "WilcoxonTestStatistic": stats,
+            "WilcoxonSignificance": pvals,
+            "WilcoxonSignificanceAdj": adj_pvals,
+        },
+    )
+
+def SubtractExpectedPatternEffect(predict_function, class_ind, X_p, X, seqsets):
+    fx_p = predict_function(X_p)
+    fx = predict_function(X)
+    patternseqs = np.array([s.split("_EPEDistribution")[0] for s in seqsets])
+    patterns = list(sorted(set([s.split("_EPEDistribution")[0] for s in seqsets])))
+    stats = []
+    pvals = []
+    ratio_pvals = []
+    adj_pvals = []
+    EPEs = []
+    for pattern in patterns:
+        ind_pattern_seqs = np.where(pattern == patternseqs)[0]
+        if len(class_ind) == 1:
+            num = fx_p[ind_pattern_seqs, class_ind[0]].reshape((-1,))
+            denom = fx[:, class_ind[0]].reshape((-1,))
+        else:
+            num = np.mean(
+                [fx_p[ind_pattern_seqs, ind] for ind in class_ind], axis=0
+            ).reshape((-1,))
+            denom = np.mean([fx[:, ind] for ind in class_ind], axis=0).reshape((-1,))
+        fc = num - denom
+        ratio_pvals.append(rank_ratio_statistic(num, denom))
+        stat, pval = wilcoxon(num, denom)
+
+        EPEs.append(np.mean(fc))
+        pvals.append(pval)
+        adj_pvals.append(pval * len(patterns))
+        stats.append(stat)
+    return (
+        fx_p,
+        fx,
+        {
+            "Pattern": patterns,
+            "ExpectedPatternEffect": EPEs,
+            "Significance": ratio_pvals,
+            "AdjustedSignificance": [r * len(patterns) for r in ratio_pvals],
+            "WilcoxonTestStatistic": stats,
+            "WilcoxonSignificance": pvals,
+            "WilcoxonSignificanceAdj": adj_pvals,
+        },
+    )
+
