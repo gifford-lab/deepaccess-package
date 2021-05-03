823f08b7f3c9191863cb120db33bde37afbb3fd9
jhammelm <jhammelm@mit.edu>
2021-05-03 17:11:40 -0400

    add transfer seed network

--- run_ASCL1vsCTCF_DeepAccess.sh
+++ run_ASCL1vsCTCF_DeepAccess.sh
@@ -1,5 +1,6 @@
 mkdir test
 
+'''
 #example train from bed file
 python src/DeepAccessTrainTransfer.py -beds  data/mm10_trimmed_ASCL1.bed data/mm10_trimmed_CTCF.bed \
        -l ASCL1 CTCF \
@@ -7,17 +8,21 @@ python src/DeepAccessTrainTransfer.py -beds  data/mm10_trimmed_ASCL1.bed data/mm
        -g default/mm10.chrom.sizes \
        -out test/ASCL1vsCTCF -nepochs 1 
 
+'''
 #example run test motif spacing on differential and ascl1 activity
 python src/DeepAccessInterpret.py -trainDir test/ASCL1vsCTCF \
        -l ASCL1 CTCF -c ASCL1vsCTCF ASCL1vsNone \
        -evalPatterns data/ASCL1_space.fa 
+'''
+python src/DeepAccessInterpret.py -trainDir test/ASCL1vsCTCF \
+       -l ASCL1 CTCF -c ASCL1vsCTCF ASCL1vsNone \
+       -fastas data/ASCL1vsCTCF_train.fa
 
 #example run test motif activity
 python src/DeepAccessInterpret.py -trainDir test/ASCL1vsCTCF \
        -l ASCL1 CTCF -c ASCL1vsCTCF \
        -evalMotifs default/HMv11_MOUSE.txt
 
-#example apply per-nucleotide saliency and make visualization plot
 python src/DeepAccessInterpret.py -trainDir test/ASCL1vsCTCF \
        -l ASCL1 CTCF -c ASCL1vsCTCF ASCL1vsNone CTCFvsNone \
        -fastas data/ASCL1vsCTCF_example.fa -saliency -vis
@@ -30,4 +35,4 @@ python src/DeepAccessTrainTransfer.py --fasta data/ASCL1vsCTCF_train.fa  \
        -ref /archive/gl/shared/genomes/mm10/mm10.fa \
        -g default/mm10.chrom.sizes  \
        -out test/ASCL1vsCTCF_from_fasta -nepochs 1 
-
+'''
