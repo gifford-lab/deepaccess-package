6e6790a058384bbf798effe85b623bdfe1402793
jhammelm <jhammelm@mit.edu>
2021-06-19 14:52:02 -0400

    add version fix

--- deepaccess/deepaccess.py
+++ deepaccess/deepaccess.py
@@ -9,20 +9,30 @@ def main():
     command_list = [train, interpret]
     command_list_str = ['train','interpret']
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", choices=command_list_str)
-    parser.add_argument("command_args",
-                        help="command arguments",
-                        nargs=argparse.REMAINDER
-                        )
-    parser.add_argument("-version", "--version",
-                        action='store_true',
-                        default=False)
+    parser.add_argument(
+        "command",
+        required=False, 
+        choices=command_list_str
+    )
+    parser.add_argument(
+        "command_args",
+        help="command arguments",
+        required=False,
+        nargs=argparse.REMAINDER
+    )
+    parser.add_argument(
+        "-version", "--version",
+        action='store_true',
+        required=False,
+        default=False
+    )
     
     opts = parser.parse_args()
 
     if opts.version:
         print('deepaccess '+deepaccess.__version__)
-    
+        exit()
+        
     command = command_list[command_list_str.index(opts.command)]
     command_args = opts.command_args
     command.main(command_args)
